9b54c4325835de3b93990e722d99f8836da66d32
Max Svensson <ad28645@skola.goteborg.se>
2026-09-07 14:26:52 +0200

    lade till .cs filen

---
+++ pseudokod_flödesschema_koa_bil/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Är du 18? (ja/nej)");
+string svar = Console.ReadLine();
+
+if (svar == "ja")
+{
+    Console.WriteLine("Har du körkort? (ja/nej)");
+    string korkort = Console.ReadLine();
+
+    if (korkort == "ja")
+    {
+        Console.WriteLine("Ja, eftersom du är över 18 år och har körkort så får du köra bil");
+    }
+    else
+    {
+        Console.WriteLine("Nej, du får inte köra bil utan körkort");
+    }
+}
+else
+{
+    Console.WriteLine("Nej, Du är inte 18 år och du får inte köra bil utan körkort");
+}
